073fc133e73eb7445e168fcd0394df40dfeca277
magic-akari <akari.ccino@gmail.com>
2023-09-10 00:44:24 +0800

    chore: add vercel scripts

---
+++ scripts/vercel_build.sh
@@ -0,0 +1,2 @@
+bun run build:wasm
+bun run build
---
+++ scripts/vercel_install.sh
@@ -0,0 +1,17 @@
+echo "Installing Rustup..."
+# Install Rustup (compiler)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# Adding binaries to path
+source "$HOME/.cargo/env"
+
+echo "Installing WASM pack..."
+# Install wasm-pack
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
+echo "Installing Bun..."
+# Install Bun
+curl -fsSL https://bun.sh/install | bash
+
+echo "Installing dependencies..."
+# Install dependencies
+bun install
