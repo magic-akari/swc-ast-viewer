b8667ca523ea338121aae1d34b33f0ef46d97928
magic-akari <akari.ccino@gmail.com>
2023-09-11 19:30:41 +0800

    perf: reduce bundle size

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import { loader } from "@monaco-editor/react";
-import { editor, languages } from "monaco-editor";
+import type { editor } from "monaco-editor";
 import monarch from "./monarch";
 import github_dark from "./themes/github_dark.json" assert { type: "json" };
 import github_light from "./themes/github_light.json" assert { type: "json" };
@@ -13,75 +13,9 @@ loader.init().then((monaco) => {
 			["[", "]"],
 			["(", ")"],
 		],
-	});
-
-	monaco.languages.registerFoldingRangeProvider("swc-ast", {
-		provideFoldingRanges(model) {
-			const text = model.getValue();
-			const result: languages.FoldingRange[] = [];
-			const lines = text.split("\n");
-			const stack = [];
-
-			for (let index = 0; index < lines.length; index++) {
-				const start = index + 1;
-				const line = lines[index];
-				const trimmed = line.trimStart();
-				if (trimmed === "span: Span {") {
-					result.push({
-						start,
-						end: start + 8,
-						kind: languages.FoldingRangeKind.Comment,
-					});
-					index += 7;
-					continue;
-				}
-
-				const indent = line.length - trimmed.length;
-				const token = trimmed.slice(-1);
-				switch (token) {
-					case "{":
-					case "[":
-					case "(":
-						stack.push({ start, indent, token });
-						break;
-					case ",": {
-						const top = stack[stack.length - 1];
-						if (top?.indent !== top.indent) {
-							continue;
-						}
-						const end = start;
-						const lookup = trimmed.slice(-2, -1);
-						if (
-							(top.token === "{" && lookup === "}") ||
-							(top.token === "[" && lookup === "]") ||
-							(top.token === "(" && lookup === ")")
-						) {
-							result.push({
-								start: top.start,
-								end,
-								kind: languages.FoldingRangeKind.Region,
-							});
-							stack.pop();
-						}
-						break;
-					}
-
-					default:
-						continue;
-				}
-			}
-
-			const last = stack.pop();
-			if (last) {
-				result.push({
-					start: last.start,
-					end: lines.length,
-					kind: languages.FoldingRangeKind.Region,
-				});
-			}
-
-			return result;
-		},
+		comments: {
+			blockComment: ["span: Span {", "},"],
+		}
 	});
 
 	monaco.languages.registerCodeLensProvider("swc-ast", {
