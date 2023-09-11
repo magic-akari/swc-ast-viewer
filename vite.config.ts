04408e9d2ef1704f58feee917302fb9d2386de64
magic-akari <akari.ccino@gmail.com>
2023-09-11 17:42:49 +0800

    chore: add vite-plugin-inspect

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { swc } from "rollup-plugin-swc3";
 import { defineConfig } from "vite";
 import externalGlobals, { libPreset } from "vite-plugin-external-globals";
+import Inspect from "vite-plugin-inspect";
 
 export default defineConfig({
 	plugins: [
+		Inspect({
+			build: true,
+			outputDir: ".vite-inspect",
+		}),
 		swc({
 			tsconfig: false,
 		}),
